8987474d8233706cf5abf58638e6b39c522888c6
Lus'ka <mihalchih@mail.ru>
2024-01-12 21:14:49 +0300

    change 12_01_24

--- Lesson2/Task1/calculator/calculator/calculator.cpp
+++ Lesson2/Task1/calculator/calculator/calculator.cpp
@@ -2,40 +2,55 @@
 
 class Calc
     {
-    public:
+    private:
     double num1;
     double num2;
 
-    double add (double n1, double n2) { 
-        double add = n1 + n2;
+    public:
+       Calc() {
+            num1 = 1;
+            num2 = 1;
+        }
+
+   bool set_num1(double num1) {
+       if (num1 != 0) {
+           this->num1 = num1;
+           return true;
+       }else return false;
+   }
+   bool set_num2(double num2) {
+       if (num2 != 0) {
+           this->num2 = num2;
+           return true;
+       }
+       else return false;
+        
+   }
+   double add () { 
+        double add = num1 + num2;
         return add;
     }
-    double multiply (double n1, double n2) {
-        double multiply = n1 * n2;
+    double multiply () {
+        double multiply = num1 * num2;
         return multiply;
     }
-    double subtract_1_2 (double n1, double n2) {
-        double subtract_1_2 = n1 - n2;
+    double subtract_1_2 () {
+        double subtract_1_2 = num1 - num2;
         return subtract_1_2;
     }
-    double subtract_2_1(double n1, double n2) {
-        double subtract_2_1 = n2 - n1;
+    double subtract_2_1() {
+        double subtract_2_1 = num2 - num1;
         return subtract_2_1;
     }
-    double divide_1_2(double n1, double n2) {
-        double divide_1_2 = n1 / n2;
+    double divide_1_2() {
+        double divide_1_2 = num1 / num2;
         return divide_1_2;
     }
-    double divide_2_1(double n1, double n2) {
-        double divide_2_1 = n2 / n1;
+    double divide_2_1() {
+        double divide_2_1 = num2 / num1;
         return divide_2_1;
     }
-    /*bool set_num1(double num1) {
-        if (num1 !=0) this->num1 = num1;
-    }
-    bool set_num2(double num2) {
-        if (num2 != 0) this->num2 = num2;
-    }*/
+   
         };
 
 int main()
@@ -43,20 +58,25 @@ int main()
     setlocale(LC_ALL, "rus");
     Calc calc;
     while (true) {
+        double num1, num2;
         std::cout << "Введите num1: ";
-        std::cin >> calc.num1;
+        std::cin >> num1;
         std::cout << "Введите num2: ";
-        std::cin >> calc.num2;
-        if (calc.num1 == 0 || calc.num2 == 0) {
+        std::cin >> num2;
+
+        if (calc.set_num1(num1)==true && calc.set_num2(num2) == true) {
+
+            std::cout << "num1 + num2 = " << calc.add() << "\n";
+            std::cout << "num1 * num2 = " << calc.multiply() << "\n";
+            std::cout << "num1 - num2 = " << calc.subtract_1_2() << "\n";
+            std::cout << "num2 - num1 = " << calc.subtract_2_1() << "\n";
+            std::cout << "num1 / num2 = " << calc.divide_1_2() << "\n";
+            std::cout << "num2 / num1 = " << calc.divide_2_1() << "\n";
+        }
+        else {
             std::cout << "Значения введены некорректно. Введите заново\n";
             continue;
-        }
-        std::cout << "num1 + num2 = " << calc.add(calc.num1, calc.num2) << "\n";
-        std::cout << "num1 * num2 = " << calc.multiply(calc.num1, calc.num2) << "\n";
-        std::cout << "num1 - num2 = " << calc.subtract_1_2(calc.num1, calc.num2) << "\n";
-        std::cout << "num2 - num1 = " << calc.subtract_2_1(calc.num1, calc.num2) << "\n";
-        std::cout << "num1 / num2 = " << calc.divide_1_2(calc.num1, calc.num2) << "\n";
-        std::cout << "num2 / num1 = " << calc.divide_2_1(calc.num1, calc.num2) << "\n";
+        };
     };
     return 0;
 }
--- Lesson2/Task2/Counter/Counter/Counter.cpp
+++ Lesson2/Task2/Counter/Counter/Counter.cpp
@@ -4,44 +4,47 @@
 class Counter
 {
 private:
-    int count=1;
+    int count;
 public:
-    int get_priv_field() { return count; } 
-    void set_begin_count(char value) { count = value; }
-    void set_priv_field(char value) { 
-        switch (value) {
-            case '+': count += 1;
-                break;
-            case '-': count -= 1;
-                break;
-            case '=': std::cout <<"Текущее значение счетчика: " << get_priv_field() << "\n";
-                break;
-            case 'x': std::cout << "До свидания!" << "\n";
-                break;
+    
+    Counter(int count) {
+        this->count = count;
     }
-    } 
+    Counter() {
+        count = 1;
+    }
+    int get_priv_field() { return count; }
+    
+    void plus() {
+        count += 1;
+    }
+    void minus() {
+        count -= 1;
+    }
+        
 };
 int main(int argc, char** argv)
 {
     setlocale(LC_ALL, "rus");
 
     Counter counter;
-    std::cout << "Вы хотите указать начальное значение счётчика? Введите +,сели да: ";
-    char ques;
-    std::cin >> ques;
-    if (ques == '+') {
-        std::cout << "\nВведите начальное значение счётчика: ";
-        int begin_count;
-        std::cin >> begin_count;
-        counter.set_begin_count(begin_count);
-    }
+  
       do
         {
         std::cout << "Введите команду('+', '-', '=' или 'x'): ";
         char x;
         std::cin >> x;
-        counter.set_priv_field(x);
-        //std::cout << counter.get_priv_field();
+            switch (x) {
+            case '+': counter.plus();
+                break;
+            case '-': counter.minus();
+                break;
+            case '=': std::cout << "Текущее значение счетчика: " << counter.get_priv_field() << "\n";
+                break;
+            case 'x': std::cout << "До свидания!" << "\n";
+                break;
+            }
+        
         if (x == 'x' || x == 'х') { break; }
         } while (true);
      
