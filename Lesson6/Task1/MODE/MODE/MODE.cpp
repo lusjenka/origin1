c04c96b5bd56c7d331df43b96a4e195bc0dae1d9
Lus'ka <mihalchih@mail.ru>
2024-01-23 00:01:46 +0300

    Lesson 6 Task1,2

---
+++ Lesson6/Task1/MODE/MODE/MODE.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+#define MODE 4
+void foo(int num1, int num2) {
+
+    cout << num1 + num2;
+
+}
+
+int main(int argc, char** argv)
+
+{
+    setlocale(LC_ALL, "rus");
+
+#if !defined MODE
+
+    std::cout << "Не определен" << std::endl;
+
+#endif
+
+#if defined MODE
+
+#if (MODE==0)
+
+    std::cout << "Работа в режиме тренировки" << std::endl;
+
+#elif (MODE==1)
+
+    std::cout << "Работа в боевом режиме" << std::endl;
+
+    int num1; int num2;
+
+    cout << "num1: " << endl;
+
+    cin >> num1;
+
+    cout << "num2: " << endl;
+
+    cin >> num2;
+
+    foo(num1, num2);
+
+#elif (MODE!=1 && MODE!=0)
+
+    std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+
+#endif 
+#endif
+}
---
+++ Lesson6/Task2/SUB/SUB/SUB.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SUB(a,b) \
+((a)-(b))
+
+
+int main(int argc, char** argv)
+
+{
+
+#if !defined SUB
+
+	std::cout << "Ne opredelen" << std::endl;
+
+#error Ne opredelen
+
+#endif
+
+#if defined SUB
+
+	int a = 6;
+	int b = 5;
+	int c = 2;
+
+	cout << SUB(a, b);
+	cout << endl;
+	cout << SUB(a, b) * c;
+	cout << endl;
+	cout << SUB(a, b + c) * c;
+
+#endif
+
+
+
+}
