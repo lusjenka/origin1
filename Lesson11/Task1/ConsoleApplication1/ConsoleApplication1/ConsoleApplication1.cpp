168216ec4f8be80df427914784d0469e2985890c
Lus'ka <mihalchih@mail.ru>
2024-02-18 17:34:11 +0300

    Lesson11

---
+++ Lesson11/Task1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::cout << "Введи точку отсчета (ТО): \n";
+    const int size = 9;
+    int to;
+    std::cin >> to;
+    int arr[size] = { 14, 16, 19, 32, 32, 32, 56, 69, 72 };
+    int left = 0;
+    int right = size - 1;
+
+   while (left < right) {
+        int middle = (left + right) / 2;
+       if (arr[middle] >= to) {
+            right = middle - 1;
+        }
+        else if (arr[middle] < to) {
+            left = middle + 1;
+        }
+    }
+   if (arr[size-1] >= to) {
+        std::cout << "\nКоличество элементов, которые больше чем ТО: " << (size-1)-left;
+    }
+    else std::cout << "\nНет элементов, которые больше чем ТО.";
+
+    return 0;
+}
