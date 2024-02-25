66216bbda694d243d87f4aaa860eca2d72715c02
Lus'ka <mihalchih@mail.ru>
2024-02-25 20:22:02 +0300

    Lesson 12

---
+++ Lesson12/Task1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int fib(int n) {
+    if (n == 1) {
+        return 0;
+    }
+    if (n == 2 || n == 3) {
+        return 1;
+    }
+    if (n > 3) {
+        return fib(n - 2) + fib(n - 1);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::cout << "Номер числа Фибоначчи: \n";
+    int n;
+    std::cin >> n;
+    if (n <= 0) {
+        std::cout << "Nevernoe chislo. ";
+        return 0;
+    }else
+    std::cout << fib(n);
+}
+
+
+
---
+++ Lesson12/Task2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+const int maxN = 100001;
+int fib(int n) {
+    static int knownF[maxN];
+    if (knownF[n] != 0) return knownF[n];
+    int t = n;
+    if (n == 1) {
+        return 0;
+    }
+    if (n == 2 || n == 3) {
+        return 1;
+    }
+    if (n > 3) {
+        t = fib(n - 2) 
+            + fib(n - 1);//почему проскакивает эту строку и записывает промежуточные результаты в массив?
+        return knownF[n] = t;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::cout << "Номер числа Фибоначчи: \n";
+    int n;
+    std::cin >> n;
+    if (n <= 0) {
+        std::cout << "Nevernoe chislo. ";
+        return 0;
+    }
+    else
+        std::cout << fib(n);
+    
+}
