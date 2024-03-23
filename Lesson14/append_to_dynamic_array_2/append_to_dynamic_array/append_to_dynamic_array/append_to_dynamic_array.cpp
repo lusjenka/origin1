3c564e3913624f757b07187946c4dcad5fc16edf
Lus'ka <mihalchih@mail.ru>
2024-03-23 20:00:14 +0300

    L14

---
+++ Lesson14/append_to_dynamic_array_2/append_to_dynamic_array/append_to_dynamic_array/append_to_dynamic_array.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+using namespace std;
+
+int* append_to_dynamic_array(int* arr, int& logical_size, int& actual_size,int& addEl)
+{
+	if (logical_size == actual_size)
+	{
+		actual_size = actual_size * 2;
+		arr[logical_size] = addEl;
+		logical_size++;
+		return arr;
+	}
+	else {
+		arr[logical_size] = addEl;
+		logical_size++;
+		return arr;
+	}
+}
+
+void add_first_dynamic_array(int* arr, int& logical_size)
+{
+	int count = 0;
+	while (count < logical_size)
+	{
+		cout << "Введите " << "arr[" << count << "]:" << endl;
+		cin >> arr[count];
+		count++;
+
+	}
+		
+};
+void print_dynamic_array(int* arr, int& logical_size, int& actual_size)
+{
+	for (int i = 0; i < logical_size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	if (actual_size > logical_size)
+	{
+		for (int i = logical_size; i < actual_size; i++)
+		{
+			cout << " _ ";
+		}
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int sizeF, sizeL,addEl;
+	cout << "Введите фактичеcкий размер массива: ";
+	cin >> sizeF;
+	cout << "Введите логический размер массива: ";
+	cin >> sizeL;
+	
+	if (sizeF < sizeL)
+	{
+		cout << "Ошибка! Логический размер массива не может превышать фактический!";
+	}
+	else {
+		int* arr = new int[sizeF];
+		add_first_dynamic_array(arr, sizeL);
+		do
+		{
+			cout << "\nВведите элемент для добавления: ";
+			cin >> addEl;
+			if (addEl == 0) { break; }
+			print_dynamic_array((append_to_dynamic_array(arr, sizeL, sizeF, addEl)), sizeL, sizeF);
+		} while (addEl != 0);
+		
+
+
+		delete[] arr;
+	}
+	
+	return 0;
+}
+
+
---
+++ Lesson14/print_dynamic_array_1/print_dynamic_array_1/print_dynamic_array_1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+void print_dynamic_array(int* arr, int logical_size, int actual_size)
+{
+	int count = 0;
+	while (count<logical_size)
+	{
+		cout << "Введите" << "arr[" << count <<"]:" << endl;
+		cin >> arr[count];
+		count++;
+		
+	}
+	for (int i = 0; i < logical_size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	if (actual_size > logical_size)
+	{
+		for (int i = logical_size; i < actual_size; i++)
+		{
+			cout << " _ ";
+		}
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int sizeF, sizeL;
+	cout << "Введите фактичеcкий размер массива: ";
+	cin >> sizeF;
+	cout << "Введите логический размер массива: ";
+	cin >> sizeL;
+	if (sizeF < sizeL)
+	{
+		cout << "Ошибка! Логический размер массива не может превышать фактический!";
+	}
+	else {
+		int* arr = new int[sizeF];
+		print_dynamic_array(arr, sizeL, sizeF);
+		delete[] arr;
+	}
+	return 0;
+}
+
