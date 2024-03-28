9ebb851b8b55026b5a2f741b727d7f1d532c1ede
Lus'ka <mihalchih@mail.ru>
2024-03-29 01:50:44 +0300

    piramid

---
+++ Lesson15/print_pyramid/print_pyramid/print_pyramid.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int left_index(int parent_index) { return 2 * parent_index + 1; }
+
+int right_index(int parent_index) { return 2 * parent_index + 2; }
+
+int parent_index(int child_index) { return (child_index - 1) / 2; }
+
+void leftOrRight(int index)
+{
+    if (index % 2 == 0)
+    {
+       cout << " right(";
+    }
+    else
+    {
+       cout << " left(";
+    }
+}
+
+int main()
+{
+    int n = 4;
+    int level = 2;
+    int k = 0;
+    int arr[10] = { 16, 11, 9, 10, 5, 6, 8, 1, 2, 4 };
+    cout << 0 << " root " << arr[0] << endl;
+
+    for (int i = 1; i < 10; i++)
+
+    {
+        if (i == 1 || i == 2) { cout << 1 << " "; }
+        else
+        {
+            if (k<n)
+            {
+             cout << level << " ";
+             k++;
+            }
+            else
+            {
+            k = 1;
+            n = n * 2;
+            level++;
+            cout << level << " ";
+            }
+        }
+        leftOrRight(i);
+        cout << arr[parent_index(i)] << ") ";
+        cout << " " << arr[i] << endl;
+    }
+    return 0;
+}
---
+++ Lesson15/travel_piramid/travel_piramid/travel_piramid.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+using namespace std;
+
+int left_index(int parent_index) { return 2 * parent_index + 1; }
+
+int right_index(int parent_index) { return 2 * parent_index + 2; }
+
+int parent_index(int child_index) { return (child_index - 1) / 2; }
+
+void leftOrRight(int index)
+{
+    if (index % 2 == 0)
+    {
+        cout << " right(";
+    }
+    else
+    {
+        cout << " left(";
+    }
+}
+
+void lev(int i, int &k, int &n, int &level)
+{
+    if (i == 1 || i == 2) { cout << 1 << " "; }
+
+    else
+    {
+        if (k < n)
+        {
+            cout << level << " ";
+            k++;
+        }
+        else
+        {
+            k = 1;
+            n = n * 2;
+            level++;
+            cout << level << " ";
+        }
+
+    }
+    
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int n = 4;
+    int level = 2;
+    int k = 0;
+    string step;
+    int arr[10] = { 16, 11, 9, 10, 5, 6, 8, 1, 2, 4 };
+    int arr_level[10] = {0,1,1,2,2,2,2,3,3,3};
+    cout << 0 << " root " << arr[0] << endl;
+
+    for (int i = 1; i < 10; i++)
+
+    {
+        lev(i, k, n, level);
+        leftOrRight(i);
+        cout << arr[parent_index(i)] << ") ";
+        cout << " " << arr[i] << endl;
+    }
+
+    cout << "Вы находитесь здесь: ";
+    cout << 0 << " root " << arr[0] << endl;
+    int i2 = 0;
+    do {
+        cout << "Введите команду: ";
+        cin >> step;
+        cout << "Ok.\n";
+        if (step == "exit") { break; }
+
+        cout << "Вы находитесь здесь: ";
+               
+        if (step == "left") { 
+            cout << arr_level[left_index(i2)] << " ";
+            leftOrRight(left_index(i2));
+            cout << arr[parent_index(left_index(i2))] << ") ";
+            cout << arr[left_index(i2)] << endl; 
+            i2 = left_index(i2);
+        }
+        else if (step == "right") { 
+            cout << arr_level[right_index(i2)] << " ";
+            leftOrRight(right_index(i2));
+            cout << arr[parent_index(right_index(i2))] << ") ";
+            cout << arr[right_index(i2)] << endl;
+            i2 = right_index(i2);
+        }
+        else if (step == "up") { 
+            cout << arr_level[parent_index(i2)] << " ";
+            leftOrRight(parent_index(i2));
+            cout << arr[parent_index(parent_index(i2))] << ") ";
+            cout << arr[parent_index(i2)] << endl;
+            i2 = parent_index(i2);
+        }
+        
+    } 
+    while (step != "exit");
+    
+    return 0;
+}
