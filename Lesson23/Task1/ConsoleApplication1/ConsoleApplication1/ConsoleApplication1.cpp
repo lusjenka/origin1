8ac9b816dca1b395adf7f3ef550e8ce9fb0f0e4d
Lus'ka <mihalchih@mail.ru>
2024-05-02 23:37:06 +0300

    l23

---
+++ Lesson23/Task1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    map <char, int> chars;
+    map <char, int>::iterator begin, at, end;
+
+    string str;
+    cout << "Ввести строку:" << endl;
+    getline(cin, str);
+
+    for (unsigned int i = 0; i < str.length(); i++) {
+        chars[str[i]]++;
+    }
+
+    vector<pair<char, int>> values(chars.begin(), chars.end());
+   
+    sort(values.begin(), values.end(), [](auto& left, auto& right) {
+        return left.second > right.second;
+        });
+
+    for (const auto& pair : values) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+      
+    return 0;
+}
---
+++ Lesson23/Task2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <forward_list>
+
+int main()
+{
+	setlocale(LC_ALL,"rus");
+	std::forward_list<int> fl;
+	std::cout << "Размер: \n";
+	int size;
+	std::cin >> size;
+	std::cout << "Cписок: \n";
+	for (int x, i = 0; i < size && std::cin >> x; i++)
+		fl.push_front(x);
+
+
+	fl.sort(); 
+
+	fl.unique();
+	fl.reverse();
+	std::cout << "\n";
+	for (const auto& elem : fl)
+		std::cout << elem << "\n";
+		
+}
