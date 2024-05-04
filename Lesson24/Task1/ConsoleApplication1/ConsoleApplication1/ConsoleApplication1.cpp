90cfcbbab480391091c6fe1b073f7c8dc79c388a
Lus'ka <mihalchih@mail.ru>
2024-05-04 18:59:24 +0300

    l24

---
+++ Lesson24/Task1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <forward_list>
+
+int main()
+{
+    std::string s;
+   
+    std::getline(std::cin, s);
+    std::stringstream ss(s);
+    std::vector<std::string> vec;
+    while (ss >> s) vec.push_back(s);
+    for (auto e : vec) std::cout << e << ",";
+    std::forward_list<std::string> fl{ vec.begin(), vec.end() };
+    fl.sort();
+    fl.unique();
+    for (auto e : fl) std::cout << e << ",";
+
+
+}
