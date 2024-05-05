9f4c2ddfad63d4513d9dff7bf37a5010ceb1aff9
Lus'ka <mihalchih@mail.ru>
2024-05-05 21:31:20 +0300

    l26_1

---
+++ Lesson26/Task1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+
+template<typename T>
+void move_vectors(std::vector<T>& lhs, std::vector<T>& rhs)
+{
+    std::vector<T> tmp = std::move(lhs);
+    lhs = std::move(rhs);
+    rhs = std::move(tmp);
+}
+
+template<typename T>
+void print_container(const T& c)
+{
+    for (const auto& e : c)
+        std::cout << e << " ";
+    std::cout << "\n";
+}
+
+int main()
+{
+    std::vector <std::string> one = { "test_string1", "test_string2" };
+    std::vector <std::string> two;
+    move_vectors(one, two);
+    print_container(two);
+
+}
+
+
