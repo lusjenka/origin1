4dc7eb43b29a1bcb8497632f86594d741b2a0d4f
Lus'ka <mihalchih@mail.ru>
2024-08-07 23:33:42 +0300

    Builder T1

---
+++ Builder/Kuk/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,81 @@
+﻿#include "map"
+#include "vector"
+#include "chrono"
+#include "iostream"
+
+struct Event {
+    std::string name;
+    std::string phone;
+    std::string message;
+    std::string description;
+    int level;
+    std::string action;
+    bool isActive;
+    std::chrono::time_point<std::chrono::steady_clock> timestamp;
+    uint8_t ttl;
+    std::string target;
+    std::string source;
+    std::vector<std::string> route;
+    std::map<std::string, std::string> data;
+    std::vector<std::string> column;
+    std::string table;
+};
+
+
+class SqlSelectQueryBuilder {
+public:
+   
+    explicit SqlSelectQueryBuilder() {
+        
+    }
+
+   
+
+    void  BuildQuery3() {
+        if (event.name.empty() && !event.column.empty()) {
+            event.name = event.column.front();
+        }
+        if (event.phone.empty() && !event.column.empty()) {
+            event.phone = event.column.back();
+        }
+        std::string y = "SELECT name, phone FROM students WHERE id=42;";
+        std::cout << y << std::endl;
+    }
+
+    
+    SqlSelectQueryBuilder& AddColumn(std::string column) noexcept {
+        event.column.push_back(column);
+        std::cout << "Add column " << column << std::endl;
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddFrom(std::string table) noexcept {
+        event.table = table;
+        std::cout << "Add table " << table << std::endl;
+        return *this;
+    }
+    
+    SqlSelectQueryBuilder& AddWhere(std::string val1, std::string val2) noexcept {
+        event.data[val1] = val2;
+        std::cout << "Add data " << val1 << " : " << val2 << std::endl;
+        return *this;
+    }
+
+private:
+    Event event;
+};
+
+
+int main() {
+  
+    SqlSelectQueryBuilder query_builder;
+    query_builder.AddColumn("name").AddColumn("phone");
+    query_builder.AddFrom("students");
+    query_builder.AddWhere("id", "42");
+
+    query_builder.BuildQuery3();
+   
+    return 0;
+}
+
+
---
+++ Kursovaya_4/Kurs4/Kurs4/Kurs4.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+#include <chrono>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+std::mutex workQueueMutex; // мьютекс, которым будем делать очередь потокобезопасной
+std::queue<std::function<void()>> workQueue; //не потокобезопасная очередь. Тип данных очереди - функция с сигнутурой void(). Будем складывать в очередь функции 
+std::atomic<bool> flagDone{ false };// флаг. Потоки будут выполняться пока флагу не присвоить занчение тру
+
+std::vector<std::thread> threadVector; //вектор потоков
+
+void func1()
+{
+    std::this_thread::sleep_for(std::chrono::microseconds(200));
+    std::cout << "Working thread id: " << std::this_thread::get_id() << " " << __FUNCTION__ << "..." << std::endl;
+}
+
+void func2()
+{
+    std::this_thread::sleep_for(std::chrono::microseconds(200));
+    std::cout << "Working thread id: " << std::this_thread::get_id() << " " << __FUNCTION__ << "..." << std::endl;
+}
+
+void addFunc1ToPool()
+{
+    while (!flagDone)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::lock_guard lockGuard{ workQueueMutex };  //для безопасности потока используем мьютекс
+        std::cout << "Add func1" << std::endl;
+        workQueue.push(func1); //каждые 500 милисекунд помещаем в нашу очередь функцию 1
+
+    }
+
+}
+
+void addFunc2ToPool()
+{
+    while (!flagDone)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::lock_guard lockGuard{ workQueueMutex }; //для безопасности потока используем мьютекс
+        std::cout << "Add func2" << std::endl;
+        workQueue.push(func2);//каждые 1000 милисекунд помещаем в нашу очередь функцию 2
+
+    }
+
+}
+
+void work()
+{
+    workQueueMutex.lock();
+    std::cout << "Start working thread id: " << std::this_thread::get_id() << std::endl;
+    workQueueMutex.unlock();
+
+    while (!flagDone || !workQueue.empty())
+    {
+        std::lock_guard lockGuard{ workQueueMutex };
+        if (!workQueue.empty())
+        {
+            auto task = workQueue.front();
+            workQueue.pop();
+            task();
+        }
+        else
+        {
+            std::this_thread::yield();
+        }
+    }
+}
+
+int main()
+{
+    std::thread addFunc1ToPoolThread{ addFunc1ToPool };//занесение функции в поток
+    std::thread addFunc2ToPoolThread{ addFunc2ToPool };//занесение функции в поток
+
+    const auto maxThreadCount{ std::thread::hardware_concurrency() }; //кол-во потоков
+
+    for (int i = 0; i < maxThreadCount - 3; i++)//минус 3 - это минус main, addFunc1ToPoolThrea, addFunc2ToPoolThrea
+    {
+        threadVector.push_back(std::thread(work));
+    }
+
+    for (auto& thread : threadVector)
+    {
+        thread.join();
+    }
+
+    addFunc1ToPoolThread.join();
+    addFunc2ToPoolThread.join();
+
+    flagDone = true;
+
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
